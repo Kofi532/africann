1c618f1db838fe9d6f616bb2747794b47e0c755c
Kofi532 <56702732+Kofi532@users.noreply.github.com>
2023-09-18 14:51:15 +0000

    Add files via upload

--- africaonline/views.py
+++ africaonline/views.py
@@ -4,4 +4,19 @@ from django.shortcuts import render
 
 
 def afriq(request):
-    return render(request, 'jot.html', {})
+    return render(request, 'home.html', {})
+
+def afriq1(request):
+    return render(request, 'about.html', {})
+
+def afriq2(request):
+    return render(request, 'gallery.html', {}) 
+
+def afriq3(request):
+    return render(request, 'faq.html', {}) 
+
+def afriq4(request):
+    return render(request, 'contact.html', {}) 
+
+def afriq5(request):
+    return render(request, 'profile.html', {}) 
